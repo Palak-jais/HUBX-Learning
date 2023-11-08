5d6431f381c8e477e5768db876dce0fd30f445f7
palak jaiswal <palakjaiswalch28@gmail.com>
2023-11-08 21:30:08 +0530

    carousel

--- src/components/reactstrap/Main.js
+++ src/components/reactstrap/Main.js
@@ -3,11 +3,13 @@ import ExampleNavbar from "./ExampleNavbar";
 import Buttons from "./Buttons";
 import Models from "./Modals";
 import DropdownPage from "./Dropdown";
+import CarouselPage from "./Carousel";
 export default function Main(){
     return <>
     <ExampleNavbar/>
     <Buttons/>
     <Models/>
     <DropdownPage/>
+    <CarouselPage/>
     </>
 }
